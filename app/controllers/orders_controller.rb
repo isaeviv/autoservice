7ea4a5f1a07090306b6d16ddcd41dfb39681c279
isaeviv <isaeviv@vivaldi.net>
2022-07-23 23:02:17 +0700

    #1 added orders

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,58 @@
+class OrdersController < ApplicationController
+  before_action :set_order, only: %i[ show edit update destroy ]
+
+
+  def index
+    @orders = Order.all
+  end
+
+
+  def show
+  end
+
+
+  def new
+    @order = Order.new
+  end
+
+
+  def edit
+  end
+
+
+  def create
+    @order = Order.new(permitted_order_params)
+
+    if @order.save
+      redirect_to order_url(@order), notice: "Order was successfully created."
+    else
+      render :new
+    end
+  end
+
+
+  def update
+    if @order.update(permitted_order_params)
+      redirect_to order_url(@order), notice: "Order was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @order.destroy
+    redirect_to orders_url, notice: "Order was successfully destroyed."
+  end
+
+  private
+
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+
+    def permitted_order_params
+      params[:order].permit(:client_name, :client_phone_number, :description)
+    end
+end
