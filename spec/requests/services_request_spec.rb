340b5165af22821b4e6b0325470e1cec09800e5d
isaeviv <isaeviv@vivaldi.net>
2022-08-25 15:19:33 +0700

    #21 added filter for services

--- spec/requests/services_request_spec.rb
+++ spec/requests/services_request_spec.rb
@@ -81,5 +81,53 @@ RSpec.describe "Services", type: :request do
     service = Service.create(name: "test", price: 1100, order: @order, specialist: @specialist)
     expect { delete "/services/#{service.id}" }.to change{ Service.count }.from(1).to(0)
   end
+  
+  describe "filtering" do
+    
+    before do
+      @category_1 = create :category, name: "Тунеядство"
+      @category_2 = create :category, name: "Починка"
+      @specialist_1 = create :specialist, name: "Васёк", profession: "Работяга"
+      @specialist_2 = create :specialist, name: "Игорёк", profession: "Столяр"
+      @order_1 = create :order, client_name: "Client_1"
+      @order_2 = create :order, client_name: "Client_2"
+      @service_1 = create :service, name: "Сходить на перекур", category: @category_1, order: @order_1, price: 100, specialist: @specialist_1
+      @service_2 = create :service, name: "Починить всё", category: @category_2, order: @order_2, price: 500, specialist: @specialist_2
+    end
+    
+    it "by specialist name" do
+      get "/services", params: {
+        specialist: @specialist_1.id
+      }
+      expect(assigns :services).to include @service_1
+      expect(assigns :services).not_to include @service_2
+    end
+    
+    it "by price" do
+      get "/services", params: {
+        price_start: "0",
+        price_end: "200"
+      }
+      expect(assigns :services).to include @service_1
+      expect(assigns :services).not_to include @service_2
+    end
+    
+    it "by category" do
+      get "/services", params: {
+        category: @category_1.id
+      }
+      expect(assigns :services).to include @service_1
+      expect(assigns :services).not_to include @service_2
+    end
+    
+    it "by order" do
+      get "/services", params: {
+        order: @order_1.id
+      }
+      expect(assigns :services).to include @service_1
+      expect(assigns :services).not_to include @service_2
+    end
+    
+  end
 
 end
