0e8cdde45ea45f23ef2ec7fe7865f0adde4d2175
isaeviv <isaeviv@vivaldi.net>
2022-11-25 10:58:37 +0500

    fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,11 +3,11 @@ import 'jquery'
 import '@popperjs/core'
 import 'bootstrap'
 import "@nathanvda/cocoon"
-import "imask"
+// import "imask"
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  let phoneFields = document.querySelectorAll("input[type=tel]");
-  phoneFields.forEach(function(field){
-    IMask(field, { mask: '+{7}(000)000-00-00' });
-  })
-})
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   let phoneFields = document.querySelectorAll("input[type=tel]");
+//   phoneFields.forEach(function(field){
+//     IMask(field, { mask: '+{7}(000)000-00-00' });
+//   })
+// })
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,5 @@ pin "application", preload: true
 pin "jquery", to: "jquery.slim.js", preload: true # @3.6.1
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
-pin "@nathanvda/cocoon", to: "https://ga.jspm.io/npm:@nathanvda/cocoon@1.2.14/cocoon.js"
-pin "imask", to: "https://ga.jspm.io/npm:imask@6.4.2/esm/index.js"
+pin "@nathanvda/cocoon", to: "@nathanvda--cocoon.js", preload: true # @1.2.14
+# pin "imask", preload: true # @6.4.3
